ebbf761dc418d859cfd4d8360f65f76ea8566484
Koly Sg <koly.sg@gmail.com>
2017-08-09 14:16:24 -0700

    changed google map ref

--- js/helper.js
+++ js/helper.js
@@ -57,7 +57,7 @@ var HTMLonlineDates = '<div class="date-text">%data%</div>';
 var HTMLonlineURL = '<br><a id = "onlineclassURL" href="#">%data%</a>';
 
 var internationalizeButton = '<button>Internationalize</button>';
-var googleMap = '<div id="map"></div>';
+var googleMap = '<div id="mapDiv"></div>';
 
 
 /*
